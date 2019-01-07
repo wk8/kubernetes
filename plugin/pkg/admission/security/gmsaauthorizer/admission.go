16f157de8375b877e6427770de97aa1b494062d7
Jean Rouge <jer329@cornell.edu>
2019-01-07 15:54:35 -0800

    WIP
    Signed-off-by: Jean Rouge <jer329@cornell.edu>

--- plugin/pkg/admission/security/gmsaauthorizer/admission.go
+++ plugin/pkg/admission/security/gmsaauthorizer/admission.go
@@ -26,7 +26,7 @@ package gmsaauthorizer
 
 import (
 	"io"
-	"k8s.io/kubernetes/staging/src/k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime"
 
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/kubernetes/staging/src/k8s.io/apiserver/pkg/admission"
